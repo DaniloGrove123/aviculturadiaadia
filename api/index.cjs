4e78a127cc006d05e3cb9983ba9a8d81bf7e86f7
DaniloGrove123 <danilogrove123@gmail.com>
2025-04-26 20:35:55 -0300

    Simplifica api/index.cjs para depurar timeout no Vercel

--- api/index.cjs
+++ api/index.cjs
@@ -1,18 +1,3 @@
-console.log('Iniciando api/index.js');
-
-const express = require('express');
-const serverless = require('serverless-http');
-
-console.log('Dependências carregadas com sucesso');
-
-const app = express();
-
-app.use(express.json());
-
-app.get('/api/hello', (req, res) => {
-  console.log('Requisição recebida em /api/hello');
-  res.json({ message: 'Hello from Avicultura Dia a Dia!' });
-});
-
-console.log('Exportando função serverless');
-module.exports = serverless(app);
+module.exports = (req, res) => {
+  res.status(200).json({ message: 'Hello from Avicultura Dia a Dia!' });
+};
